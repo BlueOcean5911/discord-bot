3671db9fedd1c03273cb2c5a31d9beccb0a09663
dev-charles <lucky.goldragon@gmail.com>
2024-11-23 06:17:01 -0500

    init

---
+++ src/bot/bot.gateway.ts
@@ -0,0 +1,31 @@
+import { InjectDiscordClient, On, Once } from "@discord-nestjs/core";
+import { Injectable, Logger, UseGuards, UseInterceptors } from "@nestjs/common";
+import { Client, Message } from "discord.js";
+
+import { MessageFromUserGuard } from "./guards/message-from-user.guard";
+import { MessageToUpperInterceptor } from "./interceptors/message-to-upper.interceptor";
+
+@Injectable()
+export class BotGateway {
+  private readonly logger = new Logger(BotGateway.name);
+
+  constructor(
+    @InjectDiscordClient()
+    private readonly client: Client
+  ) {}
+
+  @Once("ready")
+  onReady() {
+    this.logger.log(`Bot ${this.client.user.tag} was started!`);
+  }
+
+  @On("messageCreate")
+  @UseGuards(MessageFromUserGuard)
+  @UseInterceptors(MessageToUpperInterceptor)
+  async onMessage(message: Message): Promise<void> {
+    if (message.mentions.has(this.client.user)) {
+      this.logger.log(`Incoming message: ${message.content}`);
+      await message.reply("Message processed successfully");
+    }
+  }
+}
